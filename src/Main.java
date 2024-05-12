ab2af714bea8a9373cdae63061f7dc05fa4ce25e
Admin <preciouspeter3173@gmail.com>
2024-05-12 11:53:07 +0100

    Using Jframe and Jpanel

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        AddArray addArray = new AddArray();
-
-        addArray.addTempDays(0, 2);
-        addArray.addTempDays(1, 6);
-
-        addArray.findTempDays(0);
-        addArray.findTempDays(1);
-        addArray.findTempDays(3);
-
-        Scanner scanner = new Scanner(System.in);
-
-        int days =scanner.nextInt();
-
-        addArray.findTempDays(days);
+//
+//        AddArray addArray = new AddArray();
+//
+//        addArray.addTempDays(0, 2);
+//        addArray.addTempDays(1, 6);
+//
+//        addArray.findTempDays(0);
+//        addArray.findTempDays(1);
+//        addArray.findTempDays(3);
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int days =scanner.nextInt();
+//
+//        addArray.findTempDays(days);
+        
+        DeclareVariable declare = new DeclareVariable();
+        declare.findMe();
+        
+        Exercise2 exercise = new Exercise2();
 
 
     }
