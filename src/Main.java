073132c81b0593224d159af1915c9fd0f66fc837
Admin <preciouspeter3173@gmail.com>
2024-06-23 15:41:15 +0100

    heapsort added

--- src/Main.java
+++ src/Main.java
@@ -96,21 +96,50 @@ public class Main {
 //    	
 //    	linked.Search(9);
      	HashTable hash = new HashTable();
-     	  HashTable.CustomerDetails customer1 = new HashTable.CustomerDetails("John", "Doe", "1234567890", "$100", "Laptop", "Young doe");
-     	 HashTable.CustomerDetails  customer2 = new HashTable.CustomerDetails ("Jane", "Doe", "0987654321", "$200", "Phone", "Young doe");   
+     	  HashTable.CustomerDetails customer1 = new HashTable.CustomerDetails("John", "Doe", "1234567890", 50, "Laptop", "Young");
+     	 HashTable.CustomerDetails  customer2 = new HashTable.CustomerDetails ("Jane", "May", "0987654321", 20, "Iphone", "Josh"); 
+     	  HashTable.CustomerDetails customer3 = new HashTable.CustomerDetails("Lucy", "Lala", "1234567890", 30, "Iphone Painting", "Josh");
+      	 HashTable.CustomerDetails  customer4 = new HashTable.CustomerDetails ("Jack", "Poo", "0987654321", 35, "Samsung", "Peter");   
+      	HashTable.CustomerDetails customer5 = new HashTable.CustomerDetails("Larry", "Lala", "1234567890", 45, "Desktop", "Peter");
+     	 HashTable.CustomerDetails  customer6 = new HashTable.CustomerDetails ("Bark", "Poo", "0987654321", 70, "Iphone, Laptop", "Peter"); 
      	  hash.put(1, customer1);
           hash.put(2, customer2);
+          hash.put(3, customer3);
+          hash.put(4, customer4);
+          hash.put(5, customer5);
+          hash.put(6, customer6);
           
           System.out.println("Customer with key 1: " + hash.get(1)); // Should print John
           System.out.println("Customer with key 2: " + hash.get(2)); // Should print Jane
     	
-          HashTable.Seller seller1 = new HashTable.Seller("Young doe", "Product Manager");
-          HashTable.Seller seller2 = new HashTable.Seller("Old doe", "Sales Manager");
+          HashTable.Seller seller1 = new HashTable.Seller("Young", "Product Manager");
+          HashTable.Seller seller2 = new HashTable.Seller("Peter", "Sales Manager");
+          HashTable.Seller seller3 = new HashTable.Seller("Josh", "Product Manager");
+          HashTable.Seller seller4 = new HashTable.Seller("Steven", "Sales Manager");
           
           hash.putSeller(1, seller1);
+          hash.putSeller(2, seller2);
+          hash.putSeller(3, seller3);
+          hash.putSeller(4, seller4);
           System.out.println(hash.getSeller(1));
           
-          hash.CustomerAllocation("Young doe");
+          hash.CustomerAllocation("Young");
+          hash.CustomerAllocation("Steven");
+          hash.CustomerAllocation("Peter");
+          hash.CustomerAllocation("Josh");
+          
+          HashTable.Products product1 = new HashTable.Products("Iphone", 20);
+          HashTable.Products product2 = new HashTable.Products("Iphone Painting", 30);
+          HashTable.Products product3 = new HashTable.Products("Samsung", 35);
+          HashTable.Products product4 = new HashTable.Products("Laptop", 50);
+          HashTable.Products product5 = new HashTable.Products("Desktop", 45);
+          
+          hash.putProduct(1, product1);
+          hash.putProduct(2, product2);
+          hash.putProduct(3, product3);
+          
+          System.out.println(hash.getProduct(3));
+          
     
     }
 }
